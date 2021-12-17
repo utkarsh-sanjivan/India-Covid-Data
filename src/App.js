9ed42068228db8c5a44c33f26b08d436a8594c7a
Utkarsh Sanjivan <utkarsh.sannu@gmail.com>
2021-12-17 16:40:02 +0530

    Initial Deploy Attempt

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const location = useLocation();
   const pages = [
     {
-      pageLink: '/',
+      pageLink: '/India-Covid-Data/',
       view: Homepage,
       displayName: 'Homepage',
       showInNavbar: true,
