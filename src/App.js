50deaea873a91a63dbf135b4446393cc3bc8a67b
Utkarsh Sanjivan <utkarsh.sannu@gmail.com>
2022-07-05 15:40:39 +0530

    feat(google-analyitcs): Added pageview call to register user visit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { lazy, Suspense, useEffect } from 'react';
 import { Route, Redirect, Switch, useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga';
+// import ReactGA from 'react-ga';
+import ReactGA from 'react-ga4';
 
 import './App.scss';
 import { retry } from './utils/commonFunctions';
@@ -9,7 +10,9 @@ const Homepage = lazy(() => retry(() => import('./components/pages/Homepage')));
 const Header = lazy(() => retry(() => import('./components/templates/Header')));
 
 const TRACKING_ID = "UA-233194323-1";
-ReactGA.initialize(TRACKING_ID);
+const GA4_TRACKING_ID = "321226714";
+// ReactGA.initialize(TRACKING_ID);
+ReactGA.initialize(GA4_TRACKING_ID);
 
 const App = () => {
   const location = useLocation();
@@ -23,7 +26,8 @@ const App = () => {
   ];
 
   useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
+    // ReactGA.pageview(window.location.pathname + window.location.search);
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname + window.location.search });
   }, []);
 
   return (
